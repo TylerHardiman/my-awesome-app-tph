446910ea583a0550b34bdc707f48fbb9d33bd10b
TylerHardiman <Fulldadmode89@gmail.com>
2022-03-08 11:34:28 -0500

    feat: Create a simple Node "Hello World" app.

---
+++ App.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+// Simple Hello World Node App
+const APP_PORT = process.env.APP_PORT || 300; app.get('/'), (req, res) => {
+    res.send('Hello World')
+    
+};
+
+app.listen(APP_PORT, () => {
+    console.log('Running app at port:${APP_PORT}')
+})
